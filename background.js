54b16697094709ddb1b3088002bc10186c26ee70
Alison Luo <alisonluo@Alisons-MacBook-169.local>
2020-05-14 22:31:41 -0400

    Fixed button movement

--- background.js
+++ background.js
@@ -1,6 +1,12 @@
+// This will fire a popup on every new tab opened
 
+// background is non-persistent and will scan for important events to listen for
+// needs information from a persistent variable as soon as it is installed
+// listening event -- runtime.onInstalled
 chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({color: '#3aa757'}, function() {
-    console.log("The color is green.");
-  });
+    // ---> uses storage API to access a specific value
+    chrome.storage.sync.set({color: '#3aa757'}, function() {
+      console.log("The color is green.");
+    });
+
 });
--- fragment.js
+++ fragment.js
@@ -1,111 +1,184 @@
-// popup should load the recyclerview with background.js results
-// 
+// a substitute for the inline script that loads Google Maps API
+function loadScript(scriptSrc, loadedCallback) {
+  var oHead = document.getElementsByTagName("HEAD")[0];
+  var oScript = document.createElement('script');
+  oScript.type = 'text/javascript';
+  oScript.src = scriptSrc;
+  oHead.appendChild(oScript);
+  oScript.onload = loadedCallback; 
+}
 
-// reference to the ID changeColor from fragment.html
-// let changeColor = document.getElementById('changeColor');
-
-// // gets the color of changeColor element from storage
-// // change background color to that color
-// // set attribute to that color as well
-// chrome.storage.sync.get('color', function(data) {
-// 	changeColor.style.backgroundColor = data.color;
-// 	changeColor.setAttribute('value', data.color);
-// });
-
-// // make changeColor clickable
-// // when clicked, the background color of the page to the same color as the button
-// // example of programmatic injection -- lets USER control when to invoke script
-// changeColor.onclick = function(element) {
-// 	let color = element.target.value;
-// 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-// 	  chrome.tabs.executeScript(
-// 	      tabs[0].id,
-// 	      {code: 'document.body.style.backgroundColor = "' + color + '";'});
-// 	});
-// };
-// UNCOMMENT AFTER THIS
-
-// work added May 7
+// let's load the Google API js and run function main once it is done.
+loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyAwuW4NSq2HJ9WpB7gmYimPBXSBRuNGkPI&libraries=places", 
+          main);
 
 
+let pos;
 function main() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
-      var pos = {
+      pos = {
         lat: position.coords.latitude,
         lng: position.coords.longitude
       };
 
-      console.log(pos);
-
       getNearbyPlaces(pos);
     });
-  }
-  else{
+  } else {
 
   } // add else case, if browser supports geolocation and user has denied permission or not
   //see https://codelabs.developers.google.com/codelabs/google-maps-nearby-search-js/#2
 }
 
+let service;
 function getNearbyPlaces(position) {
   let request = {
     location: position,
     rankBy: google.maps.places.RankBy.DISTANCE,
-    keyword: 'sushi',
-    type:'restaurant'
+    openNow: true,
+    type: 'restaurant'
   };
 
-  // const places = new google.maps.Map(document.getElementById('map'), {
-  //           center: position,
-  //           zoom: 15
-  //         });
-
-  // service = new google.maps.places.PlacesService(places);
-  // service.nearbySearch(request, nearbyCallback);
-  const places = document.getElementById('places');
-  service = new google.maps.places.PlacesService(places);
-  //service.nearbySearch(request, nearbyCallback);
-  service.nearbySearch(request, callback);
+  map = new google.maps.Map(document.getElementById('map'));
+  service = new google.maps.places.PlacesService(map);
+  service.nearbySearch(request, nearbyCallback);
 }
 
-const results = [];
 
-// function nearbyCallback(results, status) {
-//   if (status == google.maps.places.PlacesServiceStatus.OK) {
-//     console.log("okay!");
-//     console.log(results);
-//   }
-// }
+function nearbyCallback(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    console.log(results);
+    getRandomRestaurants(results);
 
-const callback = (response, status, pagination) => {
-    if (status == google.maps.places.PlacesServiceStatus.OK) {
-        results.push(...response);
-        console.log("okay");
-        console.log(results)
+    // get coords to calculate distance from location
+    // TODO: refactor into separate fxn
+    var coordinates = [];
+    for (var i = 0; i < results.length; i++) {
+      coords = {
+        lat: results[i].geometry.location.lat(), 
+        lng: results[i].geometry.location.lng()
+      };
+      coordinates.push(coords);
+    }
+    calculate_distances(coordinates);
+  }
+}
+
+
+// pick 4 random restaurants from the results list
+const NUM_RESTAURANTS_DISPLAYED = 4;
+var rand_restaurants = [];
+function getRandomRestaurants(results) { 
+  for (var i = 0; i < NUM_RESTAURANTS_DISPLAYED; i++) {
+    get_rand(results);
+  }
+  console.log(rand_restaurants);
+
+  // make the place details request for each restaurant to be displayed
+  rand_restaurants.forEach(place => {
+    let request = {
+      placeId: place.place_id,
+      fields: ['name', 'formatted_address', 'formatted_phone_number',
+      'website', 'opening_hours', 'rating', 'price_level']
+    };
+    
+    service.getDetails(request, displayRestaurant);
+  })
+}
+
+
+let recyclerView;
+function displayRestaurant(placeResult, status) {
+  recyclerView = document.getElementById('resultsRecyclerView');
+
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    // i have no idea what html elements to actually use
+    // let results = document.createElement('p');
+    // TODO: PHONE NUMBER, DISTANCE (OUR OWN FUNCTION), OPENING HOURS, PRICE LEVEL(?)
+
+    // CREATE NAME ELEMENT
+    let name = document.createElement('h1');
+    name.classList.add('place');
+    name.textContent = placeResult.name;
+    recyclerView.appendChild(name);
+
+    // CREATE RATING ELEMENT
+    if (placeResult.rating) {
+      let rating = document.createElement('p');
+      rating.classList.add('details');
+      rating.textContent = `Rating: ${placeResult.rating}`;
+      recyclerView.appendChild(rating);
     }
 
-    if (pagination.hasNextPage) {
-        setTimeout(() => pagination.nextPage(), 2000);
+    // CREATE ADDRESS ELEMENT
+    let address = document.createElement('p');
+    address.classList.add('details');
+    address.textContent = placeResult.formatted_address;
+    recyclerView.appendChild(address);
+
+    // CREATE WEBSITE ELEMENT
+    let websitePara = document.createElement('p');
+    if (placeResult.website) {
+      let websiteLink = document.createElement('a');
+      let websiteUrl = document.createTextNode(placeResult.website);
+      websiteLink.appendChild(websiteUrl);
+      websiteLink.title = placeResult.website;
+      websiteLink.href = placeResult.website;
+      websitePara.appendChild(websiteLink);        
     } else {
-        displayResults();
+      websitePara.textContent = 'No website available';
     }
+    recyclerView.appendChild(websitePara);
+
+  }
 }
 
-const displayResults = () => {
-    results.filter(result => result.rating)
-            .sort((a, b) => a.rating > b.rating ? -1 : 1)
-     results.slice(0,3)
-            .forEach(result => {
-                places.innerHTML += `<li>${result.name} - ${result.rating}</li>`;
-            });
+
+// checks if the chosen restaurant is already in the array
+// TODO: eventually with the refresh fxn we need to reset the rand_restaurants
+function in_array(array, el) {
+   for(var i = 0 ; i < array.length; i++) 
+       if(array[i] == el) return true;
+   return false;
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-  main();
-});
+
+// pick a random restaurant from results w/o duplicating
+function get_rand(array) {  
+    var rand = array[Math.floor(Math.random()*array.length)];
+    if(!in_array(rand_restaurants, rand)) {
+       rand_restaurants.push(rand); 
+       return rand;
+    }
+    return get_rand(array);
+}
+
+
+// TODO: wondering if we could add the distances field to the results object
+function calculate_distances(coordinates) {
+  var distances = [];
+  for (i = 0; i < coordinates.length; i++) {
+    distances.push(haversine_distance(pos.lat,pos.lng,
+      coordinates[i].lat,coordinates[i].lng));
+  }
+}
+
+
+// straight line distance between two points on a spheroid 
+// approximating earth as a sphere  
+function haversine_distance(lat1, lng1, lat2, lng2) {
+  var p = 0.017453292519943295;    // Math.PI / 180
+  var c = Math.cos;
+  var a = 0.5 - c((lat2 - lat1) * p)/2 + 
+          c(lat1 * p) * c(lat2 * p) * 
+          (1 - c((lng2 - lng1) * p))/2;
+
+  return 12742 * Math.asin(Math.sqrt(a)); // 2 * R; R = 6371 km
+}
+
+
 var btnRefresh = document.getElementById('refresh').addEventListener("click", main);
-// displayResults();
-// console.log(results);
+
 
 
 
